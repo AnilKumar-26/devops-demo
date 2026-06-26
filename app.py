0f346ab37dc1616131e92a7257b806bdbd01e947
Anil <anilkumarr2564@gmail.com>
2026-06-26 11:41:22 +0800

    feat: dockerized python app with env var config

---
+++ app.py
@@ -0,0 +1,16 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+
+VERSION = os.environ.get("APP_VERSION", "1.0")
+ENV = os.environ.get("ENVIRONMENT", "local")
+
+class H(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        msg = f"Anil DevOps Engineer | version: {VERSION} | env: {ENV}"
+        self.wfile.write(msg.encode())
+    def log_message(self, *args):
+        pass
+
+HTTPServer(('', 8080), H).serve_forever()
