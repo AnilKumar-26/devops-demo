9da726260ac97e3bee6e101590c5ff7682585f4a
Anil <anilkumarr2564@gmail.com>
2026-06-26 14:27:03 +0800

    feat: capstone complete - version 2.0

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
 
-VERSION = os.environ.get("APP_VERSION", "1.0")
+VERSION = os.environ.get("APP_VERSION", "2.0")
 ENV = os.environ.get("ENVIRONMENT", "local")
 
 class H(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        msg = f"Anil DevOps Engineer | version: {VERSION} | env: {ENV}"
+        msg = f"Anil DevOps Engineer | version: {VERSION} | env: {ENV} | CAPSTONE COMPLETE"
         self.wfile.write(msg.encode())
     def log_message(self, *args):
         pass
